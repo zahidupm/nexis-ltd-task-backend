7134bf259acc44bdfbf6e2929bb7b07540ca1fb1
zahidupm <zahidhasanlimon111@gmail.com>
2022-12-05 00:44:44 +0600

    users post operation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 require('colors');
 
@@ -24,6 +25,88 @@ async function dbConnect() {
 }
 
 dbConnect();
+
+const User = client.db('nexIsFrontendTask').collection('users');
+
+function verifyJWT (req, res, next) {
+    try {
+        const authHeader = req.headers.authorization;
+        if(!authHeader) {
+            return res.status(401).send('unauthorized access')
+        }
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+            if(err) {
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            req.decoded = decoded;
+            next();
+        })
+    } catch (error) {
+        console.log(error.name.red, error.message.bold);
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+app.get('/jwt', async (req, res) => {
+    try {
+        const email = req.query.email;
+        const query = {email: email};
+        const user = await User.findOne(query);
+        if(user) {
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '7d' });
+            return res.send({ accessToken: token})
+        }
+        res.status(403).send({accessToken: ''})
+
+    }  catch (error) {
+        console.log(error.name.red, error.message.bold);
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        const user = req.body;
+        const result = await User.insertOne(user);
+        res.send(result);
+
+    } catch (error) {
+        console.log(error.name.red, error.message.bold);
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+})
+
+app.put('/users', async(req, res) => {
+    try {
+        const email = req.query.email;
+        const filter = { email: email };
+        const options = {}
+        const updatedDoc = {
+            $set: {
+                mobile_number: ''
+            }
+        }
+        const result = await User.updateOne(filter, updatedDoc, options);
+        res.send(result);
+
+    } catch(error) {
+        console.log(error.name.red, error.message.bold);
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+})
 
 app.get('/', async (req, res) => {
     res.send(`Server is running`);
